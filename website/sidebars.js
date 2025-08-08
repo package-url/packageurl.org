662f9c2acc872ffb9e85552b9baaa94b58d44ef2
Abul Hasan Sheik Madhar Ali <abulhasan@MAC-5DB33A.station>
2025-08-08 13:28:47 +0100

    Copied over website folder

---
+++ website/sidebars.js
@@ -0,0 +1,17 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  mySidebar: [
+    {
+      type: 'doc',
+      id: 'PURL_SPECIFICATION', // must match the file's frontmatter `id` or filename (see note below)
+      label: 'PURL Specification',
+    },
+    {
+      type: 'doc',
+      id: 'PURL_TYPES', // must match the file's frontmatter `id` or filename (see note below)
+      label: 'PURL Types',
+    },
+  ],
+};
+
+export default sidebars;
