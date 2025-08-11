ac574d220684d1ceac917eb2d517404908f28c90
Ralf Duli <ralfduli24@gmail.com>
2025-08-11 15:53:58 +0100

    Added website url information.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://package-url.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/packageurl.org/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Package URL', // Usually your GitHub org/user name.
-  projectName: 'Purl specs', // Usually your repo name.
+  projectName: 'packageurl.org', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
