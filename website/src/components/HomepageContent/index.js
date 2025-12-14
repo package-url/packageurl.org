639c0986845619fbc1069735d2ebf812081caa3f
johnmhoran <johnmhoran@gmail.com>
2025-12-14 14:58:42 -0800

    Restructure toolgrid #14
    Reference: https://github.com/package-url/packageurl.org/issues/14
    Signed-off-by: johnmhoran <johnmhoran@gmail.com>

--- website/src/components/HomepageContent/index.js
+++ website/src/components/HomepageContent/index.js
@@ -1,97 +1,92 @@
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import HomeInfo from './HomeInfo.mdx';
-import ToolGrid_1D from '@site/src/components/ToolGrid_1D';
+import GeneralInfo from '@site/src/components/GeneralInfo';
+// import ToolGrid_1D from '@site/src/components/ToolGrid_1D';
+import SpecGrid from '@site/src/components/SpecGrid';
+import ToolGrid from '@site/src/components/ToolGrid';
 
 import styles from './styles.module.css';
 // 2025-11-20 Thursday 14:11:38.  No longer needed?
-import getTypes from '@site/src/data/getTypes';
+// import getTypes from '@site/src/data/getTypes';
 
 export default function HomepageContent() {
     // Get baseUrl from Docusaurus context
     const { siteConfig } = useDocusaurusContext();
     const { baseUrl } = siteConfig;
-    const types = getTypes(baseUrl);
+    // const types = getTypes(baseUrl);
 
     return (
         <main>
             {/* Intro Section */}
-            <section className='container margin-vert--lg'>
-                <div className={styles.introContainer}>
+            {/* <section className='container margin-vert--lg'> */}
+            {/* <section className='container'> */}
+            <section className={styles.sectionContainer}>
+                {/* <div className={styles.introContainer}> */}
+                <div className={styles.sectionIntro}>
                     <HomeInfo />
                 </div>
             </section>
 
             {/* ToolGrid Section */}
-            <section className='styles.introContainer'>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* <section className='styles.introContainer'> */}
+            {/* <section className='container margin-vert--lg'> */}
+            {/* <section className='container'> */}
+            <section className={styles.sectionContainer}>
+                {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <h1>Software Tools</h1>
+                </div> */}
+                <div className={styles.sectionHeader} style={{ marginBottom: '15px' }}>
+                    <h1>Software Specifications and Tools</h1>
+                </div>
+                {/* <div
+                    className={styles.sectionIntro}
+                >
+                    These are community-maintained specifications and tools that
+                    support or use the Package-URL (PURL) or VERS standards.
+                </div> */}
+                {/* <ToolGrid_1D /> */}
+                <div className={styles.sectionHeader}>
+                    <h2>PURL Adoption - Specifications</h2>
                 </div>
                 <div
-                    className={styles.introContainer}
-                    style={{ paddingTop: '0', marginTop: '0' }}
+                    // className={styles.introContainer}
+                    className={styles.sectionIntro}
+                    // style={{ paddingTop: '0', marginTop: '0' }}
+                >
+                    These are specifications that have adopted PURL or VERS as
+                    part of a specification.
+                </div>
+                <SpecGrid />
+            </section>
+
+            {/* <section className='container'> */}
+            <section className={styles.sectionContainer}>
+                <div className={styles.sectionHeader}>
+                    <h2>Software Tools</h2>
+                </div>
+                <div
+                    // className={styles.introContainer}
+                    className={styles.sectionIntro}
+                    // style={{ paddingTop: '0', marginTop: '0' }}
                 >
                     These are community-maintained tools that support or use the
                     Package-URL (PURL) or VERS standards.
                 </div>
-                <ToolGrid_1D />
+                <ToolGrid />
             </section>
 
-            {/* General Info Section */}
-            <section className='container margin-vert--lg'>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <div
-                        style={{
-                            display: 'flex',
-                            padding: '2rem',
-                            paddingBottom: '0',
-                            textAlign: 'center',
-                            maxWidth: '1200px',
-                            width: '100%',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <h1>General Information</h1>
-                    </div>
+            {/* 2025-11-22 Saturday 11:16:23.  New General Information section. */}
+            {/* <section className='container'> */}
+            <section className={styles.sectionContainer}>
+                {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <h1>Software Tools</h1>
+                </div> */}
+                <div className={styles.sectionHeader}>
+                    <h1>General Information</h1>
                 </div>
-                <div className={styles.generalInfo}>
-                    {/* twoColumn layout */}
-                    <div className={styles.twoColumn}>
-                        <div className={styles.column}>
-                            <h2>Community call</h2>
-                            <div className={styles.card}>
-                                <p>
-                                    Join our next PURL community call on
-                                    2025-11-26 to discuss the status of the PURL
-                                    standard submission to Ecma.
-                                </p>
-                                <p>
-                                    The schedules for the PURL community and
-                                    TC54-TG2 calls are available at{' '}
-                                    <a
-                                        class='a_page'
-                                        href='https://calendar.google.com/calendar/u/0/embed?src=c_884decde5a152902bb51a62f89550d0f3748484534f08c63792f2e654f2a7ebc@group.calendar.google.com'
-                                        target='_blank'
-                                    >
-                                        OWASP Software Supply Chain Community
-                                        Calendar
-                                    </a>
-                                    .
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className={styles.column}>
-                            <h2>Releases</h2>
-                            <div className={styles.card}>
-                                <h3>Release 1.0.0</h3>
-                                <p>Coming soon . . .</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <GeneralInfo />
             </section>
-
         </main>
     );
 }
